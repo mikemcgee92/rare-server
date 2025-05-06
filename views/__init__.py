bd59b568a3fa3bcd04b9d1d7a1ab05bd7f261060
Ozzy Akben <akbenof@gmail.com>
2025-05-06 18:07:32 -0400

    Models & Serilizers for Posts, Categories, Tags, && Category & Tag CRUD

--- views/__init__.py
+++ views/__init__.py
@@ -1,9 +1,17 @@
-from comment_requests import (
-  get_comments_on_post,
-  get_comments_by_user,
-  get_single_comment,
-  get_all_comments,
-  create_comment,
-  update_comment,
-  delete_comment
+from views.category import (
+    create_category,
+    delete_category,
+    get_all_categories,
+    get_single_category,
+    update_category,
 )
+from views.comment_requests import (
+    create_comment,
+    delete_comment,
+    get_all_comments,
+    get_comments_by_user,
+    get_comments_on_post,
+    get_single_comment,
+    update_comment,
+)
+from views.tag import create_tag, delete_tag, get_all_tags, get_single_tag, update_tag
